0697dd85d896619fd0b91d8080a960fc008634df
joshgit <jschermann91@gmail.com>
2022-03-12 11:40:24 -0600

    fetch news api call added

--- client/src/components/FetchNews.js
+++ client/src/components/FetchNews.js
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function FetchNews() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [articles, setItems] = useState([]);
-  
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
-      // fetch(`https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=cdda6b75856c86ce1bea221c999009fa&units=imperial`)
-      fetch("https://gnews.io/api/v4/search?q=technology&token=9919faf414853020ba5e5853ccf8ce53")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result);
-            ///////////
-            console.log(result);
-            ////////
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        // <ul>
-        //   {articles.map(article => (
-        //     <li key={article.title}>
-        //       {article.title}
-        //     </li>
-        //   ))}
-        // </ul>
-        <h1></h1>
-      );
-    }
-  }
+  console.log("process.env",process.env);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [articles, setArticles] = useState([]);
 
-  export default FetchNews;
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(
+      `https://gnews.io/api/v4/search?q=technology&token=${process.env.REACT_APP_GNEWSAPI}`
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setArticles(result);
+          setIsLoaded(true);
+          console.log(result);
+          console.log(process.env);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+
+        <ul>
+          {articles.articles.map((articles) => (
+            <li key={articles.title}>{articles.title}</li>
+          ))}
+        </ul>
+
+        {/* <h1>{articles.totalArticles}</h1> */}
+      </>
+    );
+  }
+}
+
+export default FetchNews;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 // import ThoughtList from "../components/ThoughtList";
 // import FriendList from "../components/FriendList";
 // import ThoughtForm from '../components/ThoughtForm';
 import Auth from "../utils/auth";
-import FetchNews from "../components/FetchNews"
+import FetchNews from "../components/FetchNews";
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -18,9 +18,11 @@ const Home = () => {
   const loggedIn = Auth.loggedIn();
 
   return (
-    <main>
+    <>
+      {/* Leave off to avoid extra API calls */}
       <FetchNews />
-    </main>
+
+    </>
   );
 };
 
